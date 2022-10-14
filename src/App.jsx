50ffcc86a52971a772864f94b6c11b212d2aad48
Vasjan Çupri <102856709+v4sj4n@users.noreply.github.com>
2022-10-15 00:31:35 +0200

    Added Interests and About components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Info from "./components/Info";
+import About from "./components/About";
+import Interests from "./components/Interests";
 
 function App() {
   return (
     <div className="main-card">
       <Info />
+      <About />
+      <Interests />
     </div>
   );
 }
---
+++ src/components/Interests.jsx
@@ -0,0 +1,11 @@
+function Interests() {
+  return (
+    <div id="interests-div">
+      <h2>Interests</h2>
+      <p>
+        Greek, German, US music listener, Internet geek, Youtube Fanatic, research.
+      </p>
+    </div>
+  );
+}
+export default Interests;
